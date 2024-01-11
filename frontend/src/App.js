1829b3ff9c44f9a036f571f84b3e2b150c412ea7
Akbarov90 <ilyasovich706@gmail.com>
2024-01-12 04:37:01 +0500

    first page completed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import Login from '../src/auth/Login';
+import Home from './Home';
+import Verification from './auth/Verification';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home/>}/>
+                <Route path="/login" element={<Login/>}/>
+                <Route path="/verification" element={<Verification/>}/>
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
