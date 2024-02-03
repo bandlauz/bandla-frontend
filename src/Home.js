174a29abc57a9a4a9b7ecf28bef079c0fcdd8215
Azimjon Nazarov <109890132+nazarovv2@users.noreply.github.com>
2024-02-03 14:30:22 +0500

    Bootstrap uninstalled

--- src/Home.js
+++ src/Home.js
@@ -1,40 +1,37 @@
 import * as React from "react";
 import './Home.css';
 import { Link } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
-
+import Typography from "@mui/material/Typography";
+import { Box } from "@mui/system";
+import Grid from "@mui/material/Grid";
+import Button from "@mui/material/Button";
 function Home() {
     return (
-        <body>
-            <div class="p-3 mb-2 text-white">
-                <div class="row justify-content-end px-4">
-                    <div class="col-auto">
-                        <h4>
-                            <Link to="/login" style={{ textDecoration: 'none' }}>
-                                <span className="p-1 mb-1 text-white rounded"
-                                    style={{ textDecoration: 'none', backgroundColor: 'rgba(127,0,127,0.81)' }}>
-                                    Войти
-                                </span>
-                            </Link>
-                        </h4>
-                    </div>
-                </div>
-                <h3 class="message p-2">
-                    <span class="p-2 rounded text-white" style={{ backgroundColor: 'rgba(0,255,236,0.69)' }}>
-                        Hello from Bandla system 👀
-                    </span>
-                </h3>
-                <div className="image-container">
-                    <img className="centered-image"
-                        src="https://github.com/bandlauz/.github/assets/109890132/8aa4d947-9b71-45ed-b923-bce64d06df08"
-                        alt="Bandla image"/>
-                </div>
-                <h5>Jamoamizga Web-dizayner kerak🔎</h5>
-                <h5>В нашу команду нужен Веб-дизайнер🔎</h5>
-                <h5>Our team needs a Web-designer🔎</h5>
+        <div class="p-3 mb-2">
+            <Grid container justifyContent="flex-end" px={4} py={2}>
+                <Grid item>
+                    <Link to="/login" style={{ textDecoration: 'none' }}>
+                        <Button variant="contained" style={{ backgroundColor: 'rgba(127,0,127,0.81)' }}>
+                            Login
+                        </Button>
+                    </Link>
+                </Grid>
+            </Grid>
+            <h3 class="message">
+                <Box sx={{ borderRadius: '16px', backgroundColor: 'rgba(0,255,236,0.69)' }}
+                    padding={1} margin={1}>
+                    <Typography variant="h5" color="common.white">Hello from Bandla system 👀</Typography>
+                </Box>
+            </h3>
+            <div className="image-container">
+                <img className="centered-image"
+                    src="https://github.com/bandlauz/.github/assets/109890132/8aa4d947-9b71-45ed-b923-bce64d06df08"
+                    alt="Bandla image" />
             </div>
-        </body>
-    );
+            <Typography variant="h5" aalign="center" color="common.white">Jamoamizga Web-dizayner kerak🔎</Typography>
+            <Typography variant="h5" aalign="center" color="common.white" >В нашу команду нужен Веб-дизайнер🔎</Typography>
+            <Typography variant="h5" aalign="center" color="common.white">Our team needs a Web-designer🔎</Typography>
+        </div>);
 }
 
 export default Home;
