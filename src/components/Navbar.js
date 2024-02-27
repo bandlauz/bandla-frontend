7c64401ab1f3e62c7550cc3cf7a81e26cfb6952d
Azimjon Nazarov <109890132+nazarovv2@users.noreply.github.com>
2024-02-27 22:17:35 +0500

    Fixed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -48,108 +48,112 @@ function Navbar() {
     }
 
     return (
-        <Box
-            sx={{
-                marginBottom: 1,
-                p: 1
-            }}>
-            <Container>
-                <Grid container>
-                    <Grid item xs={4} align="left">
-                        <Link color="inherit" href="https://bandla.uz">
-                            <img src={process.env.PUBLIC_URL + "/logo-black.png"}
-                                alt="Bandla image"
-                                height={35}
-                            />
-                        </Link>
-                    </Grid>
-                    <Grid item xs={4}>
-                    </Grid>
-                    <Grid item xs={4} align="right">
-                        {!loggedIn ?
-                            <a href="/login" style={{ textDecoration: 'none' }}>
-                                <Button variant="contained" style={{ backgroundColor: 'rgb(0,109,199)' }}>
-                                    Войти
-                                </Button>
-                            </a> :
-                            <React.Fragment>
-                                <Tooltip title="Account settings">
+        <>
+            <Box
+                sx={{
+                    paddingTop: 0.5,
+                    marginBottom: 1
+                }}>
+                <Container>
+                    <Grid container>
+                        <Grid item xs={4} align="left">
+                            <Link color="inherit" href="https://bandla.uz">
+                                <img src={process.env.PUBLIC_URL + "/logo-black.png"}
+                                    alt="Bandla image"
+                                    height={35}
+                                />
+                            </Link>
+                        </Grid>
+                        <Grid item xs={4}>
+                        </Grid>
+                        <Grid item xs={4} align="right">
+                            {!loggedIn ?
+                                <a href="/login" style={{ textDecoration: 'none' }}>
+                                    <Button variant="contained" style={{ backgroundColor: 'rgb(0,109,199)' }}>
+                                        Войти
+                                    </Button>
+                                </a>
+                                :
+                                <React.Fragment>
                                     <IconButton
                                         onClick={handleClick}
                                         size="small"
-                                        aria-controls={open ? 'account-menu' : undefined}
-                                        aria-haspopup="true"
-                                        aria-expanded={open ? 'true' : undefined}>
+                                        sx={{
+                                            display: 'inline-block',
+                                            padding: 0,
+                                            minHeight: 0,
+                                            minWidth: 0
+                                        }}>
                                         <Avatar
                                             src={profileImg}
-                                            sx={{ width: 35, height: 35, border: '0.1px solid lightgray' }}>B</Avatar>
+                                            sx={{ width: 35, height: 35 }}>B</Avatar>
                                     </IconButton>
-                                </Tooltip>
-                                <Menu
-                                    anchorEl={anchorEl}
-                                    id="account-menu"
-                                    open={open}
-                                    onClose={handleClose}
-                                    onClick={handleClose}
-                                    PaperProps={{
-                                        elevation: 0,
-                                        sx: {
-                                            overflow: 'visible',
-                                            filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
-                                            mt: 1.4,
-                                            '& .MuiAvatar-root': {
-                                                width: 32,
-                                                height: 32,
-                                                ml: -0.5,
-                                                mr: 1,
+                                    <Menu
+                                        anchorEl={anchorEl}
+                                        id="account-menu"
+                                        open={open}
+                                        onClose={handleClose}
+                                        onClick={handleClose}
+                                        PaperProps={{
+                                            elevation: 0,
+                                            sx: {
+                                                overflow: 'visible',
+                                                filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+                                                mt: 1.4,
+                                                '& .MuiAvatar-root': {
+                                                    width: 32,
+                                                    height: 32,
+                                                    ml: -0.5,
+                                                    mr: 1,
+                                                },
+                                                '&::before': {
+                                                    content: '""',
+                                                    display: 'block',
+                                                    position: 'absolute',
+                                                    top: 0,
+                                                    right: 14,
+                                                    width: 10,
+                                                    height: 10,
+                                                    bgcolor: 'background.paper',
+                                                    transform: 'translateY(-50%) rotate(45deg)',
+                                                    zIndex: 0,
+                                                },
                                             },
-                                            '&::before': {
-                                                content: '""',
-                                                display: 'block',
-                                                position: 'absolute',
-                                                top: 0,
-                                                right: 14,
-                                                width: 10,
-                                                height: 10,
-                                                bgcolor: 'background.paper',
-                                                transform: 'translateY(-50%) rotate(45deg)',
-                                                zIndex: 0,
-                                            },
-                                        },
-                                    }}
-                                    transformOrigin={{ horizontal: 'right', vertical: 'top' }}
-                                    anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}>
-                                    <a href="/profile" style={{ textDecoration: 'none', color: "black" }}>
+                                        }}
+                                        transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+                                        anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}>
+                                        <a href="/profile" style={{ textDecoration: 'none', color: "black" }}>
+                                            <MenuItem onClick={handleClose}>
+                                                <ListItemIcon>
+                                                    <PersonIcon fontSize="small" />
+                                                </ListItemIcon>
+                                                Profile
+                                            </MenuItem>
+                                        </a>
+                                        <Divider />
                                         <MenuItem onClick={handleClose}>
                                             <ListItemIcon>
-                                                <PersonIcon fontSize="small" />
+                                                <Settings fontSize="small" />
                                             </ListItemIcon>
-                                            Profile
+                                            Settings
                                         </MenuItem>
-                                    </a>
-                                    <Divider />
-                                    <MenuItem onClick={handleClose}>
-                                        <ListItemIcon>
-                                            <Settings fontSize="small" />
-                                        </ListItemIcon>
-                                        Settings
-                                    </MenuItem>
-                                    <a href="/logout" style={{ textDecoration: 'none', color: "red" }}>
-                                        <MenuItem onClick={handleClose}>
-                                            <ListItemIcon>
-                                                <Logout fontSize="small" style={{ color: "red" }} />
-                                            </ListItemIcon>
-                                            Logout
-                                        </MenuItem>
-                                    </a>
-                                </Menu>
-                            </React.Fragment>
-                        }
+                                        <a href="/logout" style={{ textDecoration: 'none', color: "red" }}>
+                                            <MenuItem onClick={handleClose}>
+                                                <ListItemIcon>
+                                                    <Logout fontSize="small" style={{ color: "red" }} />
+                                                </ListItemIcon>
+                                                Logout
+                                            </MenuItem>
+                                        </a>
+                                    </Menu>
+                                </React.Fragment>
+                            }
+                        </Grid>
                     </Grid>
-                </Grid>
-            </Container>
-            <Divider />
-        </Box>);
+                </Container>
+                <Divider />
+            </Box>
+        </>);
 }
 
 export default Navbar;
