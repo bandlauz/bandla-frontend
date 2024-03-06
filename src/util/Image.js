9298d8b64190b1d0beab0430a494b8b6ff214fb1
Akbar <100585930+akbarjorayev@users.noreply.github.com>
2024-03-06 21:12:18 +0500

    Update ; sign in Image.js

--- src/util/Image.js
+++ src/util/Image.js
@@ -1,6 +1,6 @@
 function getData(img) {
   return new Promise((resolve, reject) => {
-    const image = new Image()
+    const image = new Image();
 
     image.onload = function () {
       resolve({
@@ -13,20 +13,20 @@ function getData(img) {
     }
 
     image.onerror = function (error) {
-      reject(error)
+      reject(error);
     }
 
-    image.src = URL.createObjectURL(img)
+    image.src = URL.createObjectURL(img);
   })
 }
 
 function maxSize(width, height) {
-  if (width === height) return 'square'
-  return width > height ? 'width' : 'height'
+  if (width === height) return 'square';
+  return width > height ? 'width' : 'height';
 }
 
 function minSize(width, height) {
-  return width < height ? 'width' : 'height'
+  return width < height ? 'width' : 'height';
 }
 
 export { getData, maxSize, minSize }
