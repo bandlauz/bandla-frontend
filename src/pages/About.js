ebfc803cf7c8066b4a2ccdab900ba6de3d4bca74
Akbar <akbarjonjorayev001@gmail.com>
2024-03-22 11:53:07 +0500

    fix -> 'Biz haqimizda' title

--- src/pages/About.js
+++ src/pages/About.js
@@ -98,7 +98,6 @@ function About() {
         </div>
         <span className="last_span">band qiling!</span>
       </div>
-      <h1 className="title">Biz haqimiqda</h1>
       <p>
         Bandla bron qilishni soddalashtiradi, bu sizga istalgan joydan joy va service band qilish imkonini beradi. Foydalanuvchi uchun qulay interfeys bilan biz jarayonni soddalashtiramiz va qulaylikni ta'minlaymiz. Konsertlar, sport tadbirlari yoki teatr tomoshalari bo'ladimi, Bandla keng ko'lamli  xizmatlardan uzluksiz foydalanish imkonini beradi. Biz bilan oson bron qiling.
       </p>
