95e8c89c04e030fbb12810a1ae9e79130eeb4fd3
Akbar <akbarjonjorayev001@gmail.com>
2024-05-12 19:58:37 +0500

    React.lazy used

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,15 +1,17 @@
+import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import './css/App.css';
-import Home from './Home';
-import Profile from './Profile';
-import Login from './Login';
-import Logout from './Logout';
-import About from './About';
-import WrongPath from './WrongPath';
-import Company from './Company';
 import Footer from '../components/Footer';
 import NavbarContainer from '../components/NavbarContainer';
-import LoginWithTelegram from '../auth/LoginWithTelegram';
+import './css/App.css';
+
+const Home = React.lazy(() => import('./Home'));
+const Login = React.lazy(() => import('./Login'));
+const LoginWithTelegram = React.lazy(() => import('../auth/LoginWithTelegram'));
+const Logout = React.lazy(() => import('./Logout'));
+const Profile = React.lazy(() => import('./Profile'));
+const About = React.lazy(() => import('./About'));
+const Company = React.lazy(() => import('./Company'));
+const WrongPath = React.lazy(() => import('./WrongPath'));
 
 function App() {
   return (
